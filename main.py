1ffb2f50bc3f1341c8dabe37e5c92641929cbd3f
ka1ut <tka1utjp@gmail.com>
2024-02-10 17:00:51 +0900

    文章を単語ベクトル化

--- main.py
+++ main.py
@@ -3,27 +3,46 @@ import matplotlib.pyplot as plt
 from sklearn.metrics.pairwise import cosine_similarity
 import spacy
 
-from database import save_vectors
+from database import save_vectors, setLabel
 from plot import plot_vectors
 
-nlp = spacy.load('ja_ginza')
-
-# ユーザーからの長いテキストを入力として受け取る
 TextInput = input("Enter a long text: ")
+
+nlp = spacy.load('ja_ginza')
 
 # テキストを文に分割
 doc = nlp(TextInput)
 sentences = [sent.text for sent in doc.sents]
+print("")
+print("---------sentences---------")
 for sentence in sentences:
     print(sentence)
+print("---------------------------")
 
-# 各文をベクトルに変換
-vectors = np.array([nlp(sentence).vector for sentence in sentences])
-data_to_save = [{"text": sentence, "vector": vector.tolist()} for sentence, vector in zip(sentences, vectors)]
+data_to_save = []
+Label = setLabel()
+for sentence in sentences:
+    doc = nlp(sentence)
+    # テキストを単語に分割
+    for token in doc:
+        # 単語のベクトルを取得し、リストに追加
+        data_to_save.append({"text": token.text, "WordClass": token.pos_, "vector": token.vector.tolist(), "label": f"{Label}"})
+
+# 各単語とそのラベルを表示
+print("")
+print("-----------word------------")
+for word in data_to_save:
+    print(f"text: {word['text']}, label: {word['label']}")
+print("---------------------------")
+
 save_vectors(data_to_save)
 
 # コサイン類似度を計算
+vectors = [item['vector'] for item in data_to_save]
 similarity_matrix = cosine_similarity(vectors)
+print("")
+print("-----similarity_matrix-----")
 print(similarity_matrix)
+print("---------------------------")
 
-plot_vectors()
+# plot_vectors()
